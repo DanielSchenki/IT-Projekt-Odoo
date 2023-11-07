ec32f7a92948f9302f1f43b12f9eba878861e8c1
mrschwarzm <schwarz.michael2001@gmail.com>
2023-11-07 15:23:23 +0100

    Manifest_File

--- addons/bmd_interface/__manifest__.py
+++ addons/bmd_interface/__manifest__.py
@@ -3,6 +3,13 @@
     'version': '1.0',
     'author': 'It-fact',
     'website': 'https://it-fact.com',
+    'category': 'Accounting',
+    'summary': 'Export von Daten für BMD',
+    'application': True,
+    'description': """
+        Export von Daten für BMD
+    """,
+    'sequence': '1',
     'depends': ['base','account','base_accounting_kit'],
     'data': [
         'views/button12.xml'
