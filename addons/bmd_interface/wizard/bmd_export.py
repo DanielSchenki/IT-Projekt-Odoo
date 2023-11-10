84a02e07ee54ac0d90bfd331009c0a71dcbc3ab4
Florian Weissörtel <k12021644@students.jku.at>
2023-11-10 20:46:31 +0100

    Account-Logik

--- addons/bmd_interface/wizard/bmd_export.py
+++ addons/bmd_interface/wizard/bmd_export.py
@@ -1,3 +1,8 @@
+import csv
+import tkinter as tk
+from tkinter import filedialog
+import subprocess
+
 from odoo import models, fields, api
 
 
@@ -5,9 +10,42 @@ class AccountBmdExport(models.TransientModel):
     _name = 'account.bmd'
     _description = 'BMD Export'
 
-    company_id = fields.Many2one(comodel_name='res.company', string="Company",
-                                 required=True)
-    period_date = fields.Date(string="Period Date", required=True)
+    period_date_from = fields.Date(string="Von:", required=True)
+    period_date_to = fields.Date(string="Bis:", required=True)
+
+    @api.model
+    def export_account(self):
+        accounts = self.env['account.account'].search([])
+
+        result_data = []
+        for acc in accounts:
+            for tax in acc.tax_ids:
+                result_data.append({
+                    'Konto-Nr': acc.code,
+                    'Bezeichnung': acc.name,
+                    'Ustcode': tax.tax_group_id.id if tax.tax_group_id else '',
+                    'USTPz': tax.amount,
+                })
+
+        # Create a Tkinter window
+        window = tk.Tk()
+
+        # Prompt the user to choose the save path
+        save_path = filedialog.asksaveasfilename(defaultextension='.csv')
+
+        # Write the data to the CSV file
+        with open(save_path, 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['Konto-Nr', 'Bezeichnung', 'Ustcode', 'USTPz']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+
+            writer.writeheader()
+            for row in result_data:
+                writer.writerow(row)
+
+        window.destroy()
+
+        return True
 
     def execute(self):
-        self.write('BMD_Export')
+        self.export_account()
+        return True
