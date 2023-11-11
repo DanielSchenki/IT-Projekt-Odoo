1d9dff298175a448dca24502df394205ef70083c
felixKroni <felixkronsteiner@gmx.at>
2023-11-11 18:34:52 +0100

    changed config

--- addons/BMD_Interface_Felix/__manifest__.py
+++ addons/BMD_Interface_Felix/__manifest__.py
@@ -1,8 +1,8 @@
 {
     'name': 'BMD_Interface_Felix',
-    'author': 'Felix Kronsteiner2',
+    'author': 'Felix Kronsteiner',
     'summary': 'An Interface from Odoo to BMD',
-    'depends': ['base','account','base_accounting_kit'],
+    'depends': ['base','account'],
     'data': [
         'views/exportBmdView.xml',
     ]
